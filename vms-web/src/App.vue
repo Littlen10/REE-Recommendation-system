6f77a099641a84814ad480a8cafeb1cc9e6b2da2
anni2022 <163743203+2024shuid@users.noreply.github.com>
2024-11-06 21:55:43 +0800

    前端平台搭建
    前端的页面基础设计与信息展示功能实现

---
+++ vms-web/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+    <index></index>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Index from "@/components/index.vue";
+
+
+export default {
+  name: 'App',
+  components: {
+    Index
+
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100%;
+}
+</style>
---
+++ vms-web/src/components/Header.vue
@@ -0,0 +1,33 @@
+<script>
+    export default {
+      name:"Header",
+      methods:{
+        toUser(){
+          console.log('to_user')
+        },
+        logout(){
+          console.log('logout')
+        }
+      }
+
+    }
+</script>
+
+<template>
+  <div style="display: flex;">
+    <div style="flex: 1;text-align: center;font-size: 30px;">
+      <span>欢迎来到登录系统</span>
+    </div>
+    <el-dropdown>
+      <span>id</span><i class="el-icon-setting" style="margin-left: 5px;"></i>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item @click.native="toUser">个人信息</el-dropdown-item>
+        <el-dropdown-item @click.native="logout">退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
